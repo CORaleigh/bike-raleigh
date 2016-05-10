98554d21f378c349cb5ab982f47168add32c030d
justingreco <justin.greco@raleighnc.gov>
2016-05-09 22:26:58 -0400

    snapToZoom false

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,7 +39,7 @@ angular.module('starter.controllers', [])
       }, constraints: {
         maxZoom: 18,
         minZoom: 9,
-        snapToZoom: false
+        snapToZoom: true
       }
     });
     MapData.setMapView(view);
